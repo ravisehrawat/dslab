bafc3a075cb56d7b9c5ab19b2c7d26e52cc7e5b5
ravisehrawat <54792904+ravisehrawat@users.noreply.github.com>
2020-06-01 20:24:17 +0530

    Create i(b).c

---
+++ i(b).c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void swap(int *x,int *y)
+{
+    int temp=*x;
+    *x=*y;
+    *y=temp;
+}
+
+void Merge(int A[],int l,int mid,int h)
+{
+     int i=l,j=mid+1,k=l;
+     int B[100];
+     while(i<=mid && j<=h)
+        {
+            if(A[i]<A[j])
+                B[k++]=A[i++];
+            else
+                B[k++]=A[j++];
+        }
+        for(;i<=mid;i++)
+            B[k++]=A[i];
+        for(;j<=h;j++)
+            B[k++]=A[j];
+        for(i=l;i<=h;i++)
+            A[i]=B[i];
+}
+
+void merge_sort(int A[],int n)
+{
+     int p,l,h,mid,i;
+      for(p=2;p<=n;p=p*2)
+      {
+           for(i=0;i+p-1<=n;i=i+p)
+           {
+                l=i;
+                h=i+p-1;
+                mid=(l+h)/2;
+                Merge(A,l,mid,h);
+           }
+      }
+      if(p/2<n)
+        Merge(A,0,p/2-1,n);
+}
+
+int main()
+{
+    int A[]={11,13,7,12,16,9,24,5,10,3},n=10,i;
+    merge_sort(A,n);
+    for(i=0;i<10;i++)
+       {
+            printf("%d ",A[i]);
+            printf("\n");
+       }
+    return 0;
+}
